a6f27c184b4e468a1a5c3269bfbc04f8b8bbf7a8
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-04-03 19:09:53 -0300

    feat: setup miragejs mock server and todo api service

---
+++ src/shared/services/api/todoApi.ts
@@ -0,0 +1,44 @@
+import axios from "axios"
+
+const axiosInstance = axios.create()
+
+type Todo = {
+    id: string,
+    label: string,
+    complete: boolean
+}
+
+type TodoWithoutId = {
+    label: string,
+    complete: boolean
+}
+
+
+export const TodoApi = {
+    async getAll() {
+        const response = await axiosInstance.get('/api/todos')
+
+        
+        return response.data.todos as Todo[]
+        
+    },
+
+    async create(data: TodoWithoutId) {
+        const response = await axiosInstance.post('/api/todos', data)
+    
+        return response.data.todos as Todo
+        
+    },
+
+    async updateById(id: string, data: Partial<TodoWithoutId>) {
+        await axiosInstance.put(`/api/todos/${id}`, data)
+    
+        return    
+    },
+
+     async deleteById(id: string) {
+        await axiosInstance.delete(`/api/todos/${id}`)
+    
+        return    
+    },
+}
