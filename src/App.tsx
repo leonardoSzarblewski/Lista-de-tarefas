ccaa2c0127474d854e296e0df73b529474b7b26e
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-04-07 23:45:19 -0300

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,38 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 import { About } from "./pages/about";
 import { Home } from "./pages/home";
 import { AppLayout } from "./shared/layout/AppLayout";
+import { Detail } from "./pages/detail";
+import { Login } from "./pages/public/Login";
 
 
 export function App() {
+  const isAuthenticad = false
 
     return (
       <BrowserRouter>
-      
+        {isAuthenticad && (
         <AppLayout>
           <Routes>
 
             <Route path="/" element={ <Home/> }/>
             <Route path="/sobre" element={ <About/> }/>
+            <Route path="/sobre" element={ <About/> }/>
+            <Route path="/detalhe/:id" element={ <Detail/> }/>
 
             <Route path="*" element={ <Navigate to='/'/> }/>
 
           </Routes>
         </AppLayout>
-      
+        )}
+
+        {!isAuthenticad && (   
+          <Routes>
+            <Route path="*" element={ <Login /> }/>
+
+          </Routes>
+        )}
+        
+
       </BrowserRouter>
     )
   } 
