b65b44d6608b53249ff08a78e3cf640e1caef50e
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-04-13 21:28:17 -0300

    feat: add logout button to header and reorganize navigation layout

--- src/shared/layout/AppLayout.tsx
+++ src/shared/layout/AppLayout.tsx
@@ -1,16 +1,25 @@
 import { NavLink } from 'react-router'
 import './AppLayout.css'
+import { useAuthContext } from '../contexts/AuthContext'
 
 export function AppLayout({ children }: React.PropsWithChildren) {
+    const { logout } = useAuthContext()
+
     return(
         <div className='layout-base'>
             <div className='layout-header'>
-                <NavLink to='/'>
-                    Página inicial
-                </NavLink>
-                <NavLink to='/sobre'>
-                    Sobre
-                </NavLink>
+                <div className='nav-wrapper'>
+                    <NavLink to='/'>
+                        Página inicial
+                    </NavLink>
+                    <NavLink to='/sobre'>
+                        Sobre
+                    </NavLink>
+                </div>
+
+                <button className='btn-logout' onClick={ logout } >
+                    Sair
+                </button>
             </div>
 
             { children }
